24e53b4e460abfbcea8985dd93c65586ab3ef1a7
Pev <pev.arfan@gmail.com>
2014-09-22 18:07:50 +0200

    End of codeigniter config

--- dev/app/config/database.php
+++ dev/app/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'dev';
+$db['default']['password'] = 'oprpg';
+$db['default']['database'] = 'oprpg';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
