b6d6ba072de01f1ca6dba774365588248abbdfea
Dumbeldor <vincentglize@hotmail.fr>
2015-04-27 17:17:48 +0200

    Add news_model

--- dev/app/models/news_model.php
+++ dev/app/models/news_model.php
@@ -4,11 +4,54 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class News_model extends CI_Model
 {
-	public function get_info()
+	protected $table = 'news';
+
+	//Add news
+	public function add_news($author, $title, $contents)
 	{
-		//	On simule l'envoi d'une requête
-		return array('auteur' => 'Chuck Norris',
-			     'date' => '24/07/05',
-		             'email' => 'email@ndd.fr');
+		$this->db->set('user_id', $author);
+		$this->db->set('new_titre', $title);
+		$this->db->set('new_message', $contents);		
+
+		$this->db->set('new_date', 'NOW()', false);
+
+		return $this->db->insert($this->table);
+	}
+
+	public function set_news($id, $title, $contents)
+	{
+		if($title == null AND $contents == null)
+			return false;
+		if($title != null)
+			$this->db->set('title', $titre);
+		if($contents != null)
+			$this->db->set('contenu', $contenu);
+
+		//Si la news est bien trouvé ! ;)
+		$this->db->where('id', (int) $id);
+		//Alors on fait l'update
+		return $this->db->update($this->table);
+	}
+	
+	public function delete_news($id)
+	{
+		return $this->db->where('id', (int) $id)
+			->delete($this->table);
+	}
+	
+	public function count($where = array())
+	{
+		return (int) $this->db->where($where)
+			      ->count_all_results($this->table);
+	}
+	
+	public function list_news()
+	{
+		return $this->db->select('*')
+			->from($this->table)
+			->limit($nb, $debut)
+			->order_by('id', 'desc')
+			->get()
+			->result();
 	}
 }
