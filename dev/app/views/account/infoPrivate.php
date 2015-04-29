7cc959f75e3ad5fa8d018dc2304661fb728eff4b
Dumbeldor <vincentglize@hotmail.fr>
2015-04-29 19:01:45 +0200

    Add -> view set Dasboard

--- dev/app/views/account/infoPrivate.php
+++ dev/app/views/account/infoPrivate.php
@@ -2,4 +2,7 @@ Identifiant : <?php echo $user['user_id']; ?> <br />
 Pseudo : <?php echo $user['user_pseudo']; ?> <br />
 Mot de passe : *********  <a href="<?php echo base_url('/account/setPassword') ?>">Modifier</a><br />
 Mail : <?php echo $user['user_email']; ?> <a href="<?php echo base_url('/account/setEmail') ?>">Modifier</a> <br />
-Menu personnalisé : ..... <a href="">Modifier</a><br />
+Dashboard : <br />
+	- Premier lien :  Forum <a href="">Modifier</a><br />
+	- Deuxième lien :  Combat <a href="">Modifier</a><br />
+	- Troisième lien :  Tchat <a href="">Modifier</a><br />
