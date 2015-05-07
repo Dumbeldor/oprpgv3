3e3c7371e999107a8e05abe354d97cdb37f2fc82
Mawloc <ta.porret@gmail.com>
2015-05-07 22:10:43 +0200

    Traduction des commentaires en anglais pour le forum  + ajouts des explications des variables pour chaque fonction pour le forum + changement pour la fonction delete_message qui ne supprime plus le message mais modifie l'attribut message_block

--- dev/app/controllers/forum.php
+++ dev/app/controllers/forum.php
@@ -1,53 +1,55 @@
 <?php
 class Forum extends MY_Controller {
 
-	/* Constructeur */
+	/* Consttructor */
 	public function __construct() {
 		parent::__construct();
-		$this->load->model('forum_model'); //Chargement du modèle MP
-		$this->load->model('users_model'); //Chargement du modèle users
-		$this->load->helper('smiley'); //Chargement des smileys
+		$this->load->model('forum_model'); //Loading of the forum's model 
+		$this->load->model('users_model'); //Loading of the users's model
+		$this->load->helper('smiley'); //Loading of the helper for the smileys
 	}
 	
-	/* Voir les différents types de forum */
+	/* Display each type of forum */
 	public function index() {
 		$data['title'] = 'Forum';
-	    $data['types'] = $this->forum_model->get_types(); //Chargement des types de forum
+	    $data['types'] = $this->forum_model->get_types(); //Loading of each forum's type
 	    $this->construct_page('forum/index', $data);
 	}
 	
-	/* Voir les différentes catégories d'un type de forum */
+	/* Display each categorie of a chosen forum */
+	/* $id_type is the chosen forum's id */
 	public function categories($id_type) {
 		$data['title'] = 'Forum';
-	    $data['categories'] = $this->forum_model->get_categories($id_type); //Chargement des catégories
+	    $data['categories'] = $this->forum_model->get_categories($id_type); //Loading of each categories
 	    $this->construct_page('forum/categories', $data);
 	}
 	
-	/* Voir les différents topics d'une des catégories */
+	/* Display each topic of a chosen categorie */
+	/* $id_cate is the chosen categorie's id */
 	public function topics($id_cate) {
 		$this->load->helper('form');
 		
 		$data['title'] = 'Forum';
-		$data['topics'] = $this->forum_model->get_topics($id_cate); // Chargement des différents topics
-		/* RAJOUTER UN get_topic_name !!!! */
+		$data['topics'] = $this->forum_model->get_topics($id_cate); // Loading of each topic (and all the information about the topic, these information will be stored in this array)
 		$data['id_categorie'] = $id_cate;
 		$this->construct_page('forum/topics', $data);
 	}
 	
-	/* Voir les différents messages d'un topic */
+	/* Display every message in a chosen topic */
+	/* $id_topic is the chosen topic's id */
 	public function messages($id_topic) {
-		$this->load->helper('form'); /* On charge l'helper afin de créer le formulaire pour aller sur la page de réponse */
+		$this->load->helper('form'); /* Loading of the form's helper */
 		
 		$data['title'] = 'Forum';
-		$data['messages'] = $this->forum_model->get_messages($id_topic); //Chargement des différents messages
+		$data['messages'] = $this->forum_model->get_messages($id_topic); //Loading of every message
 		$data['id_topic'] = $id_topic;
 		
-		$data['id_categorie'] = $this->forum_model->get_id_categorie($id_topic); //On récupère l'id de la catégorie pour que l'utilisateur puisse créer un topic dans cette catégorie
+		$data['id_categorie'] = $this->forum_model->get_id_categorie($id_topic); //Categorie's id is get in a way to enable the user to create a topic in the message's page
 		
 		$this->construct_page('forum/messages', $data);
 	}
 	
-	/* Ecrire le message de réponse */
+	/* Display the form to write an answer for a topic */
 	public function answer() {
 		$this->load->helper('form');
 		
@@ -58,14 +60,20 @@ class Forum extends MY_Controller {
 		
 	}
 	
-	/* Envoyer un message */
+	/* Send a message */
+	/*
+		$message is the text of the message
+		$date_message is the date (at that time, the timestamp) when the message is sent
+		$id_topic is the topic's id of the sent message
+		$id_user is the sender's id
+	*/
 	public function send_message($message='',$date_message='',$id_topic='',$id_user='') {
 				
 		if((!isset($message)) AND (!isset($date_message)) AND (!isset($id_topic)) AND (!isset($id_user))):
-			/* On prépare les variables pour l'envoi */
+
 			$id_topic = $this->input->post('id_topic');
 			$message = $this->input->post('message');
-			$date_message = date('Y-m-d', time()); //Date au format AAAA-MM-JJ
+			$date_message = date('Y-m-d', time()); //Date in YYYY-MM-DD format
 			$session = $this->session->all_userdata();
 			$id_user = $session['user_data']['user_id'];
 		endif;
@@ -75,15 +83,16 @@ class Forum extends MY_Controller {
 		$this->messages($id_topic);
 	}
 	
-	/* Supprimer un message */
+	/* Delete a message */
+	/* $id_message is the message's id that will be deleted */
 	public function delete_message($id_message) {
-		$id_topic = $this->forum_model->get_id_topic($id_message); //On récupère l'id du topic pour rediriger l'utilisateur vers le topic après la suppression du message
+		$id_topic = $this->forum_model->get_id_topic($id_message); //Topic's id is get in order to redirect the user to this topic 
 		$this->forum_model->delete_message($id_message);
 				
 		$this->messages($id_topic[0]['topic_id']);
 	}
 	
-	/* Créer un nouveau topic */
+	/* Create a new topic */
 	public function create_topic() {
 		$this->load->helper('form');
 		
@@ -93,17 +102,17 @@ class Forum extends MY_Controller {
 		$this->construct_page('forum/create_topic', $data);
 	}
 	
-	/* Envoi du nouveau topic */
+	/* Send a new topic */
 	public function send_topic() {
 		
 		$id_categorie = $this->input->post('id_categorie');
 		$topic_name = $this->input->post('topic_name');
 		
-		$id_topic = $this->forum_model->send_topic($id_categorie,$topic_name); //On récupère l'id du topic créé tout en l'envoyant
+		$id_topic = $this->forum_model->send_topic($id_categorie,$topic_name); //Topic's id is get in a way to redirect the user to this topic
 		
 		$session = $this->session->all_userdata();
 		$message = $this->input->post('message');
-		$date_message = date('Y-m-d', time()); //Date au format AAAA-MM-JJ
+		$date_message = date('Y-m-d', time()); //Date in YYYY-MM-DD format
 		
 		$this->send_message($message,$date_message,$id_topic[0]['topic_id'],$session['user_data']['user_id']);
 		
--- dev/app/models/forum_model.php
+++ dev/app/models/forum_model.php
@@ -1,61 +1,82 @@
 <?php
 class Forum_model extends CI_Model {
 	
-	//Constructeur
+	//Constructor
 	public function __construct() {
 		$this->load->database();
 	}
 	
-	/* Retourne les différents types de forums */
+	/* Return each type of forums */
 	public function get_types() {
 		$query = $this->db->query('SELECT * FROM forums_types');
 		return $query->result_array();
 	}
 	
-	/* Retourne les différentes catégories */
+	/* Return each categories from a specific forum */
+	/* $id_type is the chosen forum's id */
 	public function get_categories($id_type) {
 		$query = $this->db->query('SELECT * FROM forums_categories WHERE forum_id = ?', array($id_type));
 		return $query->result_array();
 	}
 	
-	/* Retourne les différents topics */
+	/* Return each topics from a specific categories */
+	/* $id_cate is the chosen cate's id */
 	public function get_topics($id_cate) {
 		$query = $this->db->query('SELECT * FROM forums_topics WHERE cate_id = ?', array($id_cate));
 		return $query->result_array();
 	}
 	
-	/* Retourne les différents messages */
+	/* Return each messages from a specific topic */
+	/* $id_topic is the chosen topic's id */
 	public function get_messages($id_topic) {
 		$query = $this->db->query('SELECT u.user_pseudo, f.message, f.topic_id, f.message_date, f.message_id FROM forums_topics_messages f, users u WHERE f.topic_id = ? AND f.user_id = u.user_id ORDER BY message_id LIMIT 25', array($id_topic));
 		return $query->result_array();
 	}
 	
-	/* Envoi un message */
+	/* Send a message */
+	/* 
+		$id_topic is the topic's id where the message will be sent
+		$message is the message that will be sent
+		$date_message is the date when the message is sent
+		$user_id is the user's id who send the message
+	*/
 	public function send_message($id_topic,$message,$date_message,$user_id) {
 		$this->db->insert('forums_topics_messages', array('message'=>$message,'message_date'=>$date_message,'topic_id'=>$id_topic,'user_id'=>$user_id));
 	}
 	
-	/* Retourne l'id du topic d'un message afin de pouvoir rediriger l'utilisateur vers le topic quand il poste un message (par exemple) */
+	/* Return the id of a topic, in a way to load this topic when the user send a message */
+	/* $id_message is the message's id wich will be used in order to return topic's id */
 	public function get_id_topic($id_message) {
 		$query = $this->db->query("SELECT topic_id FROM forums_topics_messages WHERE message_id = ?", array($id_message));
 		return $query->result_array();
 	}
 	
-	/* Retourne l'id de la catégorie pour envoyer le topic dans la bonne catégorie si l'utilisateur créé un topic sur la page des messages */
+	/* Return the id of a categorie to load the categorie if the user just created a topic in the message's page */
+	/* $id_topic is the topic's id which will be used in a way to return categorie's id */
 	public function get_id_categorie($id_topic) {
 		$query = $this->db->query("SELECT cate_id FROM forums_topics WHERE topic_id = ?", array($id_topic));
 		return $query->result_array();
 	}
 	
-	/* Supprime un message */
+	/* Delete a message (does'nt really delete it, just stops displaying it, in order to keep it in the database) */
+	/* $id_message is the message's id of the message that will be deleted */
 	public function delete_message($id_message) {
-		$this->db->delete('forums_topics_messages', array('message_id'=>$id_message));
+		$this->db->query('UPDATE forums_topics_messages SET message_block=true WHERE message_id=?', array($id_message));
 	}
 	
-	/* Création du nouveau topic et retourne l'id du topic */
+	/* Creation of a new topic */
+	/* 
+		$id_categorie is the categorie's id of the categorie where the topic will be send
+		$topic_name is the name of the topic
+	*/
 	public function send_topic($id_categorie,$topic_name) {
-		//$this->db->insert('forums_topics', array('topic_name'=>$topic_name,'cate_id'=>$id_categorie));
-		$query = $this->db->query("SELECT topic_id FROM forums_topics WHERE topic_name = ? AND cate_id = ? ORDER BY topic_id DESC LIMIT 1", array($topic_name,$id_categorie));
+		$this->db->insert('forums_topics', array('topic_name'=>$topic_name,'cate_id'=>$id_categorie));
+		$query = $this->db->query(
+			"SELECT topic_id 
+			FROM forums_topics 
+			WHERE topic_name = ? AND cate_id = ? 
+			ORDER BY topic_id DESC LIMIT 1", 
+			array($topic_name,$id_categorie));
 		return $query->result_array();
 	}
 }
