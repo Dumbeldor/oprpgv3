9d8dfde1b8878ec499ad3a824a3aa7a803e6d50d
Dumbeldor <vincent.glize@live.fr>
2015-05-24 19:17:40 +0200

    Add messaging skelette

---
+++ dev/app/controllers/messaging.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ ******************************************************************************
+ * Description : 
+ * This page contains the messaging handling
+ * @todo Coding
+ * @author Dumbeldor
+ * @version 0.1.0
+ ******************************************************************************
+ */
+class Messaging extends MY_Controller {
+    // ========================================================================
+    // CREATE PAGE
+    // ========================================================================
+
+    /**
+     * Default Constructor
+     * ----------------------------------------------------------------------- */
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('messaging');
+    if(!$this->users_model->is_connected())
+      redirect('index');
+  }
+    /**
+     * Of the messaging index page, list private message
+     * ----------------------------------------------------------------------- */
+  public function index()
+  {
+    $data['title'] = 'Messagerie';
+    $data['private_message'] = $this->messaging->lists(5, 0);
+    $this->construct_page('messagerie/index', $data);
+  }
+  /*
+  * Read a private message
+  */
+  public function read()
+  {
+    $data['title'] = 'Messagerie';
+    
+    
+    $this->construct_page('messagerie/messagerie', $data);
+  }
+}
