afcc571c3463ee8a763e33e1aa4b0ec445f60d3e
Dumbeldor <vincent.glize@live.fr>
2015-06-27 16:32:17 +0200

    Fix error news

--- dev/app/views/pages/home.php
+++ dev/app/views/pages/home.php
@@ -5,10 +5,10 @@
     <h1>Les news</h1>
     <?php if($nbnews <= 0) { ?>
     	<p>Il y a aucune news<br />
-    	<a href="<?php echo base_url('news/add/'); ?>">Ajouter une news</a></p>
+    	<a href="<?php echo base_url('/news/add/'); ?>">Ajouter une news</a></p>
     <?php } else { 
-    	if ($admin) { ?>
-    		<a href="<?php echo base_url('news/add/'); ?>">Ajouter une news</a> <br /><?php } ?>
+    	if ($connecte &&$admin) { ?>
+    		<a href="<?php echo base_url('/news/add/'); ?>">Ajouter une news</a> <br /><?php } ?>
     	<?php echo $pagination; ?>
     	<?php foreach($news as $new): ?>
     	<h2><?php echo $new->title; ?></h2>
@@ -17,7 +17,7 @@
     	if($connecte && $admin) { ?>
     			<a href="<?php echo base_url(array('news', 'delete', $new->id)); ?>">Supprimer la news</a> 
     	<?php } ?></p>
-    	<a href="<?php echo base_url('news/comment/'.$new->id);?>">Commentaire</a>
+    	<a href="<?php echo base_url('/news/comment/'.$new->id);?>">Commentaire</a>
     	</br>
     	<p><?php echo $new->message; ?></p>
     	<br />
