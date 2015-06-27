419f744c57ee83724d75521f7283c798e19a34d7
Dumbeldor <vincent.glize@live.fr>
2015-06-27 17:29:05 +0200

    Fix error in View user info

--- dev/app/views/users/view.php
+++ dev/app/views/users/view.php
@@ -1,9 +1,9 @@
-<?php var_dump($users); ?> </br>
-</br>
+<br />
+<br />
 Avatar :  <br /> <br />
 Pseudo : <?php echo $users['pseudo']; ?> <br />
 Lvl : <?php echo $users['lvl'];?> <br />
-<a href=<?php echo base_url("messaging/write/".$users['pseudo']."");?>>Envoyer un message</a><br />
+<a href=<?php echo base_url("/messaging/write/".$users['pseudo']."");?>>Envoyer un message</a><br />
 Rang : <?php echo $users['rank']; ?> <br />
 Nombre de news �crites : <?php echo $users['nb_news']; ?> <br />
 Nombre de message post�s : <?php echo $users['nb_mess_forum']; ?> <br />
