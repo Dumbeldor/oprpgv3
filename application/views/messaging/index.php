6179cbd38ce0949219c4971e58e355fce7930009
Vincent <vincent.glize@live.fr>
2015-10-30 18:34:55 +0100

    Fix messagerie

--- application/views/messaging/index.php
+++ application/views/messaging/index.php
@@ -34,12 +34,12 @@
 				<?php  if($message['is_read'] == 0) {//If private message not read
 				       ?><strong>Message non lu</strong> <?php
 				}
-				?>De : <?php echo htmlentities($message['pseudo']) .' le '. date('d/m/Y à H\hi',$message['date_time']); ?>
+				?>De : <?php echo $message['pseudo'] .' le '. date('d/m/Y à H\hi',$message['date_time']); ?>
 				<br />
 			
 				<?php 
 				//Displays the beginning of the message and displays a link if the member wants to read the entire message
-				echo $message['content']; ?></em></strong></blockquote></u> 
+				echo htmlentities($message['content']); ?></em></strong></blockquote></u> 
 				<br />
 				<a href="<?php echo base_url("/messaging/read/".$message['id']); ?>">Lire la suite</a>
 				</div>
