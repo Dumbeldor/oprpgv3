34b54e4fff9a1783dad968fe9a1817be58091f0a
Vincent <vincent.glize@live.fr>
2015-11-01 14:46:21 +0100

    Correction js

--- assets/js/tchat/tchat.js
+++ assets/js/tchat/tchat.js
@@ -21,7 +21,7 @@ function update_tchat() {
 	  } else {
 	    supp = '';
 	  }
-       html += '<div class="tchat_message"><div class="row"><div class="columns small-3">'
+       html += '<div class="tchat_message"><div class="columns small-12"><div class="row"><div class="columns small-3">'
 	  + '<div><strong><a href="'+url_user+'/'+message.userId+'" class="'+message.ranks+'">' +message.pseudo
 	  + '</a></strong></div><div class="message_tchat_milieu"><img height="74px" width="74px" src="'+img_url+'/avatarDefault.png"></img></div>'
 	  + '</div><div class="columns small-9 messageTchat panel" id="'+message.ranks+'"><div class="message_tchat_haut" id="'+message.ranks+'"">'
@@ -30,7 +30,7 @@ function update_tchat() {
 	  + supp
 	  + '</div><div class="message_tchat_milieu">'
 	  + message.message
-	  + '</div><div class="message_tchat_bas"></div></div></div></div>';
+	  + '</div><div class="message_tchat_bas"></div></div></div></div></div>';
     });
     $('#tchat_container').html(html);
   });
