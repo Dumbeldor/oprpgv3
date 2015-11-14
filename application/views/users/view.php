154a8a21e63ec7d0c716d12bb8acbe544ea1a909
Vincent <vincentglize@hotmail.fr>
2015-11-14 17:34:55 +0100

    Echappement caractere spéciaux

--- application/views/users/view.php
+++ application/views/users/view.php
@@ -2,11 +2,11 @@
     <br />
     <img src="<?php echo base_url('assets/img/avatarsJoueurs/'.$id.'.png');?>"></img><br>	<br />
     Pseudo : <a href="#" class="<?= $users['rank']?>"><?php echo $users['pseudo']; ?></a> <br />
-    Faction : <?= htmlentities($users['facName']); ?><br>
+    Faction : <?= $users['facName']; ?><br>
     Lvl : <?php echo $users['lvl'];?> <br />
     Rang : <?php echo $users['rank']; ?> <br />
     Nombre message forum : <?php echo $users['messNumber']; ?> <br />
-    Équipage : <a href="<?php echo base_url('crews/view/'.$users['crewId']);?>"><?php echo $users['crewName'];?></a><br />
+    Équipage : <a href="<?php echo base_url('crews/view/'.$users['crewId']);?>"><?php echo htmlentities($users['crewName']);?></a><br />
     Rang dans l'équipage : <?php echo $users['crewGrade'];?><br><br>
     Inscription : <?= date('d/m/Y à H\hi',$users['registration'])?><br>
     Dernière connexion : <?= date('d/m/Y à H\hi',$users['last_connection'])?><br>
